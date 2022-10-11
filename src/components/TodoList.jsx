1d7c9eca1acacbe14f9b37be2a44fc5d51af0063
Hossain Sany <hossainsany@Hossains-MacBook-Air.local>
2022-10-11 18:10:59 +0600

    typo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -5,7 +5,7 @@ export default function TodoList({ todos, clickHandle }) {
         return (
             <div className="single-todo" key={todo.id}>
                 <h2>{todo.title}</h2>
-                <span>At {todo.date}</span>
+                <span>{todo.date}</span>
                 <p>{todo.details}</p>
                 <button className="close-btn" onClick={() => clickHandle(todo.id)}>
                     X
