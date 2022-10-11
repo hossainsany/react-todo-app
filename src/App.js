436694cec5dbe49cb967cca3f89d06eefb87e74d
Hossain Sany <hossainsany@Hossains-MacBook-Air.local>
2022-10-11 18:07:35 +0600

    added form functionality

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import TodoList from "./components/TodoList";
 function App() {
     const [showingTodos, setShowingTodos] = useState(true);
     const [showingModal, setShowingModal] = useState(false);
-    const [todos, setTodos] = useState([
-        { title: "Go Shopping", details: "Shop for peanut butter and toothpaste", date: "11/10/2022", id: 1 },
-        { title: "Code a little", details: "Create a todo app in react", date: "11/10/2022", id: 2 },
-        { title: "Do nothing", details: "Do absolutely nothing for rest of the day", date: "11/10/2022", id: 3 },
-    ]);
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+        setTodos((prevTodo) => {
+            return [...prevTodo, todo];
+        });
+    };
 
     const clickHandle = (id) => {
         setTodos(todos.filter((todo) => id !== todo.id));
@@ -22,20 +24,20 @@ function App() {
     const modalHandle = () => (showingModal ? setShowingModal(false) : setShowingModal(true));
 
     return (
-        <div className='App'>
+        <div className="App">
             <h1>Todo App</h1>
-            <div className='btn-container'>
+            <div className="btn-container">
                 {!showingTodos && (
-                    <button className='btn' onClick={todoHandle}>
+                    <button className="btn" onClick={todoHandle}>
                         Show Todos
                     </button>
                 )}
                 {showingTodos && (
-                    <button className='btn' onClick={todoHandle}>
+                    <button className="btn" onClick={todoHandle}>
                         Hide Todos
                     </button>
                 )}
-                <button className='btn' onClick={modalHandle}>
+                <button className="btn" onClick={modalHandle}>
                     Add New Todos
                 </button>
             </div>
@@ -43,7 +45,7 @@ function App() {
             {showingModal && (
                 <Modal modalHandle={modalHandle}>
                     {" "}
-                    <AddTodo />
+                    <AddTodo modalHandle={modalHandle} addTodo={addTodo} />
                 </Modal>
             )}
         </div>
--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import "./AddTodo.css";
 
-export default function AddTodo() {
+export default function AddTodo({ modalHandle, addTodo }) {
+    const [title, setTitle] = useState("");
+    const [details, setDetails] = useState("");
+    const [date, setDate] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const todo = {
+            title: title,
+            details: details,
+            date: date,
+            id: Math.floor(Math.random() * 10000),
+        };
+        addTodo(todo);
+        console.log(todo.id);
+        //reset form
+        setTitle("");
+        setDetails("");
+        setDate("");
+        modalHandle();
+    };
+
     return (
         <>
             <h2 className="todo-title">Add New Todo</h2>
-            <form className="add-todo-form">
-                <input type="text" placeholder="Title" />
-                <input type="text" placeholder="Description" />
-                <input type="date" />
+            <form className="add-todo-form" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Title" onChange={(e) => setTitle(e.currentTarget.value)} value={title} />
+                <input type="text" placeholder="Description" onChange={(e) => setDetails(e.currentTarget.value)} value={details} />
+                <input type="date" onChange={(e) => setDate(e.currentTarget.value)} value={date} />
                 <button className="btn">Add</button>
             </form>
         </>
