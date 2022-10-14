f03cc2f9bbb2193616fa347eb9b106ff68712d2a
Hossain Sany <hossainsany3@gmail.com>
2022-10-15 04:53:26 +0600

    update 15.10.22

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,25 @@
+import { useEffect, useRef } from "react";
 import "./Modal.css";
 
 export default function Modal({ children, modalHandle }) {
-    return (
-        <div className="modal-backdrop">
-            <div className="modal">
-                {children}
-                <button className="close-btn" onClick={modalHandle}>
-                    X
-                </button>
-            </div>
-        </div>
-    );
+	let menuRef = useRef();
+
+	useEffect(() => {
+		document.addEventListener("mousedown", (event) => {
+			if (!menuRef.current.contains(event.target)) {
+				modalHandle();
+			}
+		});
+	});
+
+	return (
+		<div className="modal-backdrop">
+			<div className="modal" ref={menuRef}>
+				{children}
+				<button className="close-btn" onClick={modalHandle}>
+					X
+				</button>
+			</div>
+		</div>
+	);
 }
